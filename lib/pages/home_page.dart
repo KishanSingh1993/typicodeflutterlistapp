db3e9de06815a90262584b9bed8f363729a5bcfd
shivam.gupta9 <kishang.90@gmail.com>
2024-06-19 17:35:13 +0530

    initial

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,44 +7,39 @@ import 'package:typicode_listapp/controllers/post_controller.dart';
 
 import 'details_page.dart';
 
-class HomePage extends StatelessWidget{
-
+class HomePage extends StatelessWidget {
   final PostController postController = Get.put(PostController());
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Posts'),
       ),
-     body: Obx(() {
-
-       if(postController.isLoading.value){
-
-         return const Center(child: CircularProgressIndicator());
-       }
-       else if(postController.postList.isEmpty){
-         return const Center(child: Text('No post found'),);
-       }
-       else{
-
-         return ListView.builder(
-             itemCount: postController.postList.length,
-             itemBuilder: (context, index){
-               return ListTile(
-                 title: Text(postController.postList[index].title),
-                 onTap: (){
-                   Get.to (()=> DetailsPage(postId: postController.postList[index].id));
-                 },
-               );
-             }
-
-         );
-       }
-
-     }),
-   );
+      body: Obx(() {
+        if (postController.isLoading.value) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (postController.postList.isEmpty) {
+          return const Center(child: Text('No posts found'));
+        } else {
+          return ListView.builder(
+            itemCount: postController.postList.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  ListTile(
+                    title: Text(postController.postList[index].title),
+                    onTap: () {
+                      Get.to(() => DetailsPage(postId: postController.postList[index].id));
+                    },
+                  ),
+                  const Divider(),  // Add divider here
+                ],
+              );
+            },
+          );
+        }
+      }),
+    );
   }
-
 }
